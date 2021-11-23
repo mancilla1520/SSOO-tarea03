4153349d529f75c2f8bba1dd515a670639122bdd
Jose Mancilla <josemancillamarambio@gmail.com>
2021-11-22 22:32:58 -0300

    Readme y captura de señales

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SSOO-tarea03
+José Mancilla 20.476.565-0 jose.mancilla@alumnos.uv.cl
+
+El programa inicia declarando las variables de control para el while, el cual se encargara de dar las 50 iteraciones en la sucesion de fibonacci, para luego en un proceso hijo ir capturando los numeros impares que vayan saliendo de la iteracion incial.
+
+Para que no existan conflictos se uso la funcion .join()
+Para capturar la señal (ctcl + c), se utilizo signal, para capturarla, imprimir un mensaje de que no se puede parar y siga hasta que se den las 50 iteraciones.
+
+Se agrego un sleep de medio segunda para cada iteracion, simplemente para hacerlo mas ordenado.
+
+
 Tarea Sistemas Operativos
--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,26 +1,41 @@
 //Jose Mancilla 20.476.565-0
 
 
-#include<iostream>
+#include <iostream>
 #include <thread>
-
+#include <conio.h>
+#include <csignal>
 using namespace std;
+
+
+void siginthandler(int param)
+{
+    printf("No se puede parar \n");
+
+}
+
 
 void func(int n){
    if ((n % 2)!=0)
     {
-        cout << "Impar: "<< n << " PID: "<< getpid <<  endl;
-       
+        cout << "Impar: "<< n <<  endl;
+    
     }
 
 }
 
 
-int main() {
-    int a=0, b=1, c=0, contador=0, limite = 5, ContadorPar=0;
 
-	
+
+
+int main() {
+    int a=0, b=1, c=0, contador=0, limite = 50, ContadorPar=0;
+    
+
+
     while (contador <= limite) {
+
+        
         cout <<"fibonacci: " << c << " PID: "<<getpid << endl;
         _sleep(500);
         a = b;
@@ -29,13 +44,12 @@ int main() {
         contador = contador+1;
         thread t1(func, ref(c));
         t1.join();
-        
-        
-
-
-    }
+        signal(SIGINT, siginthandler);
 
     
+
+    
+}
     return 0;
 
 
